8f1b4abd3a4d81e6bcc38b6a5c9021d435a24615
Pushpan Nresht <30312779+nreshtpushpan@users.noreply.github.com>
2026-04-12 20:11:51 +0530

    Add: asteroid-collision - submission-2

---
+++ Data Structures & Algorithms/asteroid-collision/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> st;
+        for(auto a: asteroids) {
+            while(!st.empty() && a < 0 && st.back() > 0) {
+                int diff = a + st.back();
+                if(diff < 0) {
+                    st.pop_back();
+                } else if(diff > 0) {
+                    a = 0;
+                } else {
+                    a = 0;
+                    st.pop_back();
+                }
+            }
+            if(a != 0) st.push_back(a);
+        }
+        return st;
+    }
+};
