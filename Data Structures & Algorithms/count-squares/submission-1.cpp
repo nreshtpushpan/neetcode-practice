2bb1b4c69556fbd9f073fc2dbb87054e41785003
Pushpan Nresht <30312779+nreshtpushpan@users.noreply.github.com>
2026-05-09 15:58:42 +0530

    Add: count-squares - submission-1

---
+++ Data Structures & Algorithms/count-squares/submission-1.cpp
@@ -0,0 +1,28 @@
+class CountSquares {
+public:
+    map<pair<int,int>, int> mp;
+    unordered_map<int, unordered_set<int>> xAxis;
+    CountSquares() {
+        
+    }
+    
+    void add(vector<int> point) {
+        int x = point[0];
+        int y = point[1];
+        mp[{x,y}]++;
+        xAxis[x].insert(y);
+    }
+    
+    int count(vector<int> point) {
+        int x1 = point[0];
+        int y1 = point[1];
+        int res = 0;
+        for(auto y2 : xAxis[x1]) {
+            int dis = abs(y2-y1);
+            if(dis == 0) continue;
+            res += mp[{x1-dis, y2}] * mp[{x1-dis, y1}] * mp[{x1,y2}] ;
+            res += mp[{x1+dis, y2}] * mp[{x1+dis, y1}] * mp[{x1,y2}];
+        }
+        return res;
+    }
+};
