1f87010c136b4511fc30030934413d0fba142c52
Pushpan Nresht <30312779+nreshtpushpan@users.noreply.github.com>
2026-05-10 21:37:01 +0530

    Add: capacity-to-ship-packages-within-d-days - submission-1

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-1.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int countDaysWithCapacity(const vector<int>& weights, const int &capacity) {
+        int days = 0;
+        int sum = 0;
+        for(auto w : weights) {
+            if(sum + w > capacity) {
+                ++days;
+                sum = w;
+            } else {
+                sum += w;
+            }
+        }
+        if(sum) ++days;
+        return days;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int n = (int)weights.size();
+        int totalWeight = 0, maxPackage = 0;
+        for(auto w : weights) {
+            totalWeight += w;
+            maxPackage = max(maxPackage, w);
+        }
+        int l = maxPackage, r = totalWeight;
+        int minW = totalWeight;
+        while(l <= r) {
+            int m = (r-l)/2 + l;
+            int daysReq = countDaysWithCapacity(weights, m);
+            if(daysReq <= days) {
+                minW = min(minW, m);
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return minW;
+    }
+};
