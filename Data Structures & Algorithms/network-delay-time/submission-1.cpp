6b316ab803d5ed67a471d1ef8a75b07dbb843e10
Pushpan Nresht <30312779+nreshtpushpan@users.noreply.github.com>
2026-05-14 20:51:36 +0530

    Add: network-delay-time - submission-1

---
+++ Data Structures & Algorithms/network-delay-time/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        if(n == 1) return 0;
+        vector<vector<pair<int,int>>> g(n+1);
+        for(auto edge : times) {
+            g[edge[0]].push_back({edge[1], edge[2]});
+        }
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+        int totalRequiredNodes = n-1;
+        for(auto node : g[k]) {
+            pq.push({node.second, node.first});
+        }
+        vector<bool> vis(n+1, false);
+        vis[k] = true;
+        while(!pq.empty()) {
+            auto [timeTaken, node] = pq.top();
+            pq.pop();
+            if(vis[node]) continue;
+            vis[node] = true;
+            if(--totalRequiredNodes == 0) return timeTaken;
+            for(auto child: g[node]) {
+                pq.push({child.second + timeTaken, child.first});
+            }
+        }
+        return -1;
+    }
+};
