e76d27abfa144c48bae530ac64e1453f1bba9640
Pushpan Nresht <30312779+nreshtpushpan@users.noreply.github.com>
2026-05-14 13:46:57 +0530

    Add: min-cost-to-connect-points - submission-3

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-3.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = (int)points.size();
+        vector<int> disMap(n, INT_MAX);
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+        
+        for(int j = 1; j < n; j++) {
+            int dis = abs(points[0][0] - points[j][0]) + abs(points[0][1] - points[j][1]);
+            pq.push({dis, j});
+            disMap[j] = dis;
+        }
+        
+        vector<bool> vis(n, false);
+        vis[0] = true;
+        int totalCost = 0;
+        while(!pq.empty()) {
+            auto [dis, index] = pq.top();
+            pq.pop();
+            if(vis[index]) continue;
+            vis[index] = true;
+            totalCost += dis;
+            for(int i = 0; i < n; i++) {
+                if(vis[i]) continue;
+                int dis = abs(points[i][0] - points[index][0]) + abs(points[index][1] - points[i][1]);
+                if(dis < disMap[i]) {  // ✅ only push if cheaper
+                    pq.push({dis, i});
+                    disMap[i] = dis;
+                }
+            }
+        }
+        return totalCost;
+    }
+};
