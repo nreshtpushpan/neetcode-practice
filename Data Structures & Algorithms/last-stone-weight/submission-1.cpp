a27bb0e83cec2efa3fbfa2a5d266c87afc7cd321
Pushpan Nresht <30312779+nreshtpushpan@users.noreply.github.com>
2026-05-15 19:09:24 +0530

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        int n = (int)stones.size();
+        for(int i = 0; i < n; i++) pq.push(stones[i]);
+        while(pq.size() >= 2) {
+            auto x = pq.top();
+            pq.pop();
+            auto y  = pq.top();
+            pq.pop();
+            if(x == y) continue;
+            if(x < y) pq.push(y-x);
+            else pq.push(x-y);
+        }
+        return (!pq.empty()) ? pq.top() : 0;
+    }
+};
