77359e35c4acf6f677d7659a8c4bc986ecd92a7a
Pushpan Nresht <30312779+nreshtpushpan@users.noreply.github.com>
2026-05-20 17:37:41 +0530

    Add: word-ladder - submission-6

---
+++ Data Structures & Algorithms/word-ladder/submission-6.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_map<string, int> dic;
+        int n = (int)wordList.size();
+        for(int i = 0; i < n; i++) dic[wordList[i]] = i;
+        int len = 1;
+        queue<pair<string, bitset<100>>> q;
+        q.push({beginWord, 0});
+        while(!q.empty()) {
+            int siz = q.size();
+            for(int k = 0; k < siz; k++) {
+                auto [word, hash] = q.front();
+                q.pop();
+                if(word == endWord) return len;
+                int m = (int)word.size();
+                for(int i = 0; i < m; i++) {
+                    int ch = word[i];
+                    for(char changeCh = 'a'; changeCh <= 'z'; changeCh++) {
+                        word[i] = changeCh;
+                        auto it = dic.find(word);
+                        if(it == dic.end()) continue;
+                        int index = it->second;
+                        if(hash.test(index)) continue;
+                        hash.set(index);
+                        q.push({word, hash});
+                        hash.reset(index);
+                    }
+                    word[i] = ch;
+                }
+            }
+            ++len;
+        }
+        return 0;
+    }
+};
