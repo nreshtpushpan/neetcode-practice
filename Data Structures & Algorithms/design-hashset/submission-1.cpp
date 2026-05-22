5f89ef9c5e461792d8d57e62188ac9165fd60649
Pushpan Nresht <30312779+nreshtpushpan@users.noreply.github.com>
2026-05-22 22:43:58 +0530

    Add: design-hashset - submission-1

---
+++ Data Structures & Algorithms/design-hashset/submission-1.cpp
@@ -0,0 +1,26 @@
+class MyHashSet {
+public:
+    bitset<100001> hash;
+    MyHashSet() {
+    }
+    
+    void add(int key) {
+        hash.set(key);
+    }
+    
+    void remove(int key) {
+        hash.reset(key);
+    }
+    
+    bool contains(int key) {
+        return hash.test(key);
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
