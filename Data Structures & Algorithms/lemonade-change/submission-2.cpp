c08edcc394d0bcdf117aa69778868623422d20b2
Pushpan Nresht <30312779+nreshtpushpan@users.noreply.github.com>
2026-05-23 10:09:38 +0530

    Add: lemonade-change - submission-2

---
+++ Data Structures & Algorithms/lemonade-change/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0, ten = 0;
+        for(auto bill : bills) {
+            if(bill == 5) five++;
+            else if(bill == 10) {
+                if(!five) return false;
+                five--; ten++;
+            } else {
+                if(ten && five) { ten--; five--; }
+                else if(five >= 3) { five -= 3; }
+                else return false;
+            }
+        }
+        return true;
+    }
+};
